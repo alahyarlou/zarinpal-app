7f641e61c1df3dab85438cdbac4b965cc3dcfb47
ali alahyarlou <alialahyarlou@gmail.com>
2022-09-17 22:29:40 +0430

    create project and install tailwindcss librery & add font's & Images to assets folder

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["IRANYekanWeb"],
+      YekanBakh: ["YekanBakh"],
+    },
+  },
+  plugins: [],
+};
